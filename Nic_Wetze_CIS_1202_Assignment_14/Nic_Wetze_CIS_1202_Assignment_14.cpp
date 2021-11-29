389ff0c9638cbacb8602dd0c57f08c51406489c4
Nicholas Wetzel <tattoosbynic@gmail.com>
2021-11-29 15:12:08 -0500

    Added code for the main method so that the program actually runs through every class of vehicle and gathers required user input. Most of the program still needs commented though

--- Nic_Wetze_CIS_1202_Assignment_14/Nic_Wetze_CIS_1202_Assignment_14.cpp
+++ Nic_Wetze_CIS_1202_Assignment_14/Nic_Wetze_CIS_1202_Assignment_14.cpp
@@ -3,10 +3,58 @@
 // November 25, 2021
 
 #include <iostream>
+#include <string>
+#include "Vehicle.cpp"
+#include "Car.cpp"
+#include "Truck.cpp"
+
 
 using namespace std;
 
 int main()
 {
-    
+    string manufacturer;
+    int yearBuilt;
+    int numDoor;
+    int towCapacity;
+
+    cout << "Vehicle Program\n" << endl;
+
+    cout << "\nVehicle: " << endl;
+    cout << "Enter the manufacturer: ";
+    getline(cin, manufacturer);
+
+    cout << "Enter the year built: ";
+    cin >> yearBuilt;
+
+    Vehicle vehicle(manufacturer, yearBuilt);
+    vehicle.displayInfo();
+
+    cout << "\nCar: " << endl;
+    cout << "Enter the manufacturer: ";
+    getline(cin, manufacturer);
+
+    cout << "Enter the year built: ";
+    cin >> yearBuilt;
+
+    cout << "Enter the number of doors: ";
+    cin >> numDoor;
+
+    Car car(manufacturer, yearBuilt, numDoor);
+    car.displayInfo();
+
+    cout << "\nTruck: ";
+    cout << "Enter the manufacturer: ";
+    getline(cin, manufacturer);
+
+    cout << "Enter the year built: ";
+    cin >> yearBuilt;
+
+    cout << "Enter the towing capacity: ";
+    cin >> towCapacity;
+
+    Truck truck(manufacturer, yearBuilt, towCapacity);
+    truck.displayInfo();
+
+    return 0;
 }
